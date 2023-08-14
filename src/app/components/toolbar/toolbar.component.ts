6cbb2bf0936b35db9c943f4c3cebc608cada27df
Geovanni Rojas <geovanny118@hotmail.com>
2023-08-14 08:40:34 -0500

    Se agrega botones al toolbar

--- src/app/components/toolbar/toolbar.component.ts
+++ src/app/components/toolbar/toolbar.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
-import { CommonModule } from '@angular/common';
+import { MatButtonModule } from '@angular/material/button';
+import { MatToolbarModule } from '@angular/material/toolbar';
 
 @Component({
   selector: 'app-toolbar',
   standalone: true,
-  imports: [CommonModule],
+  imports: [MatToolbarModule, MatButtonModule],
   templateUrl: './toolbar.component.html',
   styleUrls: ['./toolbar.component.scss']
 })
