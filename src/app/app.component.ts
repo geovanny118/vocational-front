ac424192ce4fb8672b03ea5fa260ff34634a6253
Geovanni Rojas <geovanny118@hotmail.com>
2023-08-14 14:23:37 -0500

    Se se declara la seccion para el inicio

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,7 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
-import { ToolbarComponent } from './components';
+import { ToolbarComponent } from './shared/components';
 
 @Component({
   selector: 'app-root',
