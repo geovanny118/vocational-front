38f19d4f979812324d3a78a8bd56d0a227397e9b
Geovanni Rojas <geovanny118@hotmail.com>
2024-02-03 15:28:04 -0500

    Se ajusta redireccion en botones de home

--- src/app/modules/home/home-routing.module.ts
+++ src/app/modules/home/home-routing.module.ts
@@ -1,6 +1,5 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { CommonModule } from '@angular/common';
 import { HomeComponent } from '.';
 
 const routes: Routes = [
--- src/app/modules/user/user-routing.module.ts
+++ src/app/modules/user/user-routing.module.ts
@@ -1,12 +1,13 @@
 import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
+import { RouterModule, Routes } from '@angular/router';
+import { UserComponent } from '.';
 
-
+const routes: Routes = [
+  { path: '', component: UserComponent },
+];
 
 @NgModule({
-  declarations: [],
-  imports: [
-    CommonModule
-  ]
+  imports: [RouterModule.forChild(routes)],
+  exports: [RouterModule]
 })
 export class UserRoutingModule { }
