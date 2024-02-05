11b664091a90d7879aea8bf7ee2ce43309df9bbd
Geovanny <geovanny118@hotmail.com>
2024-02-04 23:29:30 -0500

    Se agrega icono y titulo en el toolbar de la app

--- src/app/shared/components/toolbar/toolbar.component.ts
+++ src/app/shared/components/toolbar/toolbar.component.ts
@@ -1,19 +1,36 @@
 import { Component, inject } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
+import { NavigationEnd, Router, RouterModule, Routes } from '@angular/router';
 import { MatButtonModule } from '@angular/material/button';
 import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatIconModule } from '@angular/material/icon';
 import { AuthenticationService } from 'src/app/modules/authentication/services';
 import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-toolbar',
   standalone: true,
-  imports: [MatToolbarModule, MatButtonModule, RouterModule, CommonModule],
+  imports: [MatToolbarModule, MatButtonModule, RouterModule, CommonModule, MatIconModule],
   templateUrl: './toolbar.component.html',
   styleUrls: ['./toolbar.component.scss']
 })
 export class ToolbarComponent {
   authenticationService: AuthenticationService = inject(AuthenticationService);
+  showTitle: boolean = true;
+  router = inject(Router);
+
+  ngOnInit() {
+    // Suscribirse al evento NavigationEnd para obtener notificaciones cuando cambia la URL
+    this.router.events.subscribe((event) => {
+      if (event instanceof NavigationEnd) {
+        // Obtener la URL actual
+        const currentUrl = this.router.url;
+        // URL específica que quieres comparar
+        const urlHome = '/home';
+        // Lógica para decidir si mostrar o no los elementos basándote en la URL
+        this.showTitle = currentUrl === urlHome;
+      }
+    });
+  }
 
   isLoggedIn(): boolean {
     return this.authenticationService.isLoggedIn();
