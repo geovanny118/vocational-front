dcd0f9a09dc02e532025d313b92a548ecb9f76a2
Geovanni Rojas <geovanny118@hotmail.com>
2024-03-18 21:51:35 -0500

    Se crea la vista para editar usuario

--- src/app/modules/user/components/edit-form/edit-form.component.ts
+++ src/app/modules/user/components/edit-form/edit-form.component.ts
@@ -1,12 +1,88 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { NgClass, NgIf } from '@angular/common';
+import { Router, RouterModule } from '@angular/router';
+import { AbstractControl, FormBuilder, FormGroup, ReactiveFormsModule, ValidationErrors, ValidatorFn, Validators } from '@angular/forms';
+import { MatIconModule } from '@angular/material/icon';
+import { MatInputModule } from '@angular/material/input';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatButtonModule } from '@angular/material/button';
+import { User } from '../../models';
+import { UserService } from '../../services';
+import { AuthenticationService } from 'src/app/modules/authentication/services';
+import { forkJoin } from 'rxjs';
 
 @Component({
   selector: 'app-edit-form',
   standalone: true,
-  imports: [],
+  imports: [MatFormFieldModule, MatInputModule, MatIconModule, MatButtonModule, ReactiveFormsModule, RouterModule, NgClass, NgIf],
   templateUrl: './edit-form.component.html',
   styleUrl: './edit-form.component.scss'
 })
 export class EditFormComponent {
+  status: string = 'init';
+  hidePassword: boolean = true;
+  hideConfirmPassword: boolean = true;
+  
+  user: User | undefined;
+  authenticationServices: AuthenticationService = inject(AuthenticationService);
+  userServices: UserService = inject(UserService); 
 
+  editForm: FormGroup = this._formBuilder.group({
+    nombres: ['', [Validators.required]],
+    apellidos: ['', [Validators.required]],
+    telefono: ['', [Validators.required]],
+    direccion: ['', [Validators.required]],
+    ciudad: ['', [Validators.required]],
+    email: ['', [Validators.email, Validators.required]],
+  });
+
+  constructor(
+    private _formBuilder: FormBuilder,
+    private _router: Router,
+  ) { }
+
+  ngOnInit(): void {
+    const userId = localStorage.getItem('identificacion');
+    if (userId) {
+      forkJoin([
+        this.authenticationServices.getLoggedInUserInfo(userId),
+        this.userServices.getUserInfo(userId)
+      ])
+        .subscribe({
+          next: ([authenticationResponse, userResponse]) => {
+            this.authenticationServices.currentUserSignal.set(authenticationResponse);
+            this.user = userResponse;
+          },
+          error: (error) => {
+            console.error('Error fetching user information:', error);
+          }
+        });
+    }
+  }
+
+  submit() {
+    if (this.editForm.valid) {
+      this.status = 'loading';
+      const user: User = this.editForm.getRawValue();
+
+      /*
+      console.log(user);
+      // llama al servicio para el registro, si es correcto redirige a la pantalla de login
+      this._authenticationServices.registration(user).subscribe({
+        next: () => { this._router.navigate(['/authentication/login']) },
+        error: () => { this.status = 'failed'; }
+      });
+
+      // limpia el formulario despues de enviar la informacion
+      this.editForm.reset();
+      this.editForm.markAsPristine();
+      this.editForm.markAsUntouched();
+      Object.keys(this.editForm.controls).forEach(key => {
+        this.editForm.get(key)?.setErrors(null);
+      });
+      */
+    } else {
+      this.editForm.markAllAsTouched();
+    }
+  }
 }
