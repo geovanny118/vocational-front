745390706288e33283542ed37af784dee0bbd650
Geovanni Rojas <geovanny118@hotmail.com>
2024-03-24 17:57:46 -0500

    Se crea estructura, servicios, paginas, componentes para el test de holland

---
+++ src/app/modules/holland-test/pages/test-application/test-application.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-test-application',
+  standalone: true,
+  imports: [],
+  templateUrl: './test-application.component.html',
+  styleUrl: './test-application.component.scss'
+})
+export class TestApplicationComponent {
+
+}
