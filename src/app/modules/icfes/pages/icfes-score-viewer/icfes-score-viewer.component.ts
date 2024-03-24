09a762f7dd156de3f1fe1673bb4cd3cf99884435
Geovanni Rojas <geovanny118@hotmail.com>
2024-03-24 16:20:01 -0500

    Se crea modelos

--- src/app/modules/icfes/pages/icfes-score-viewer/icfes-score-viewer.component.ts
+++ src/app/modules/icfes/pages/icfes-score-viewer/icfes-score-viewer.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { PdfUploadFormComponent } from '../../components';
+import { MatTableModule } from '@angular/material/table';
 
 @Component({
   selector: 'app-icfes-score-viewer',
   standalone: true,
-  imports: [PdfUploadFormComponent],
+  imports: [PdfUploadFormComponent, MatTableModule],
   templateUrl: './icfes-score-viewer.component.html',
   styleUrl: './icfes-score-viewer.component.scss'
 })
