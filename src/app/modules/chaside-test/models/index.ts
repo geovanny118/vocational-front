c77713e3367e38d3857f3097034e7552b74f128f
Geovanni Rojas <geovanny118@hotmail.com>
2024-06-08 18:51:33 -0500

    Adiciona imagenes y texto de interes
    Se ajusta la funcion para generar imagenes y texto para los resultados de chaside text

--- src/app/modules/chaside-test/models/index.ts
+++ src/app/modules/chaside-test/models/index.ts
@@ -1,3 +1,5 @@
 export * from './cards-universidades';
 export * from './chaside-questions';
 export * from './chaside-result';
+export * from './imagenes-area-interes';
+export * from './texto-area-interes';
