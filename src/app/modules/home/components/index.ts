ad1cb4163ec52b0f52972befd44b3f76b8763eed
Lumar <luis.yepes.molina@gmail.com>
2024-06-08 18:42:16 -0500

    ajuste pagina de inicio y cambio de plantilla a Angular

---
+++ src/app/modules/home/components/index.ts
@@ -0,0 +1,5 @@
+export * from './body/body.component';
+export * from './header/header.component';
+export * from './informacion/informacion.component';
+export * from './servicios/servicios.component';
+export * from './test/test.component';
