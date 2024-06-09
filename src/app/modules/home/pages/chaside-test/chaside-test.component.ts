f8e737e3c5ab93a8e9d7dd0b40ee8d9c5601b9be
Geovanni Rojas <geovanny118@hotmail.com>
2024-06-08 21:22:11 -0500

    Se ajusta ruta para los botones en el la pagina home

---
+++ src/app/modules/home/pages/chaside-test/chaside-test.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-chaside-test',
+  standalone: true,
+  imports: [],
+  templateUrl: './chaside-test.component.html',
+  styleUrl: './chaside-test.component.scss'
+})
+export class ChasideTestComponent {
+
+}
---
+++ src/app/modules/home/pages/index.ts
@@ -0,0 +1,2 @@
+export * from './chaside-test/chaside-test.component';
+export * from './holland-test/holland-test.component';
