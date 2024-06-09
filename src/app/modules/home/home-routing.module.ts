b5ad1dc503c68b5d27dc5a8867e6065525779868
Geovanni Rojas <geovanny118@hotmail.com>
2024-06-08 21:23:22 -0500

    Se ajusta ruta para los botones en el la pagina home

--- src/app/modules/home/home-routing.module.ts
+++ src/app/modules/home/home-routing.module.ts
@@ -1,9 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from '.';
+import { ChasideTestComponent, HollandTestComponent } from './pages';
 
 const routes: Routes = [
   { path: '', title: 'Inicio', component: HomeComponent },
+  { path: 'chaside', title: 'Chaside test', component: ChasideTestComponent },
+  { path: 'holland', title: 'Holland test', component: HollandTestComponent },
 ];
 
 @NgModule({
