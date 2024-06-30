733793e13118adcac9de6affb54c230c357e492a
Geovanni Rojas <geovanny118@hotmail.com>
2024-06-29 21:11:45 -0500

    Se crea la vista para el modulo de descarga de informes

--- src/app/modules/report/report.component.ts
+++ src/app/modules/report/report.component.ts
@@ -1,12 +1,35 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
+import { MatTableModule } from '@angular/material/table';
+import { NgFor } from '@angular/common';
+import { Usuario } from '../authentication/models';
+import { AuthenticationService } from '../authentication/services';
 
 @Component({
   selector: 'app-report',
   standalone: true,
-  imports: [],
+  imports: [NgFor, MatButtonModule, MatTableModule],
   templateUrl: './report.component.html',
   styleUrl: './report.component.scss'
 })
 export class ReportComponent {
+  user: Usuario | undefined;
+  authenticationServices = inject(AuthenticationService);
+  displayedColumns: string[] = ['#', 'Tipo de informe', 'Acciones'];
+  reports: string[] = ['Usuarios', 'Universidades', 'Preguntas'];
+
+  ngOnInit(): void {
+    const userId = localStorage.getItem('identificacion');
+    if (userId) {
+      this.authenticationServices.getLoggedInUserInfo(userId).subscribe({
+        next: (response) => {
+          this.authenticationServices.currentUserSignal.set(response);
+        },
+        error: () => {
+          this.authenticationServices.currentUserSignal.set(null);
+        }
+      });
+    }
+  }
 
 }
--- src/app/modules/report/report.module.ts
+++ src/app/modules/report/report.module.ts
@@ -3,7 +3,6 @@ import { CommonModule } from '@angular/common';
 import { ReportRoutingModule } from './report-routing.module';
 
 
-
 @NgModule({
   declarations: [],
   imports: [
