f4315dc131e7d3f264b7a0c9eb6193fe3d6de572
Geovanni Rojas <geovanny118@hotmail.com>
2024-08-17 15:27:04 -0500

    Se ajuste redireccion cuando se expira el token

--- src/app/modules/authentication/services/authentication.service.ts
+++ src/app/modules/authentication/services/authentication.service.ts
@@ -4,6 +4,8 @@ import { environment } from '@environments/environment';
 import { Observable } from 'rxjs/internal/Observable';
 import { LoginCredentials, Usuario, UsuarioAutenticado } from '../models';
 import { Router } from '@angular/router';
+import { SessionExpiredDialogService } from '.';
+
 
 @Injectable({
   providedIn: 'root'
@@ -12,6 +14,7 @@ export class AuthenticationService {
   private _httpClient: HttpClient = inject(HttpClient);
   private _baseUrl: string = environment.apiBaseUrl;
   private _router: Router = inject(Router);
+  private _sessionExpiredDialogService: SessionExpiredDialogService = inject(SessionExpiredDialogService);
 
   // undefined: estado inicial | null: no autorizado | Usuario: logueado
   currentUserSignal = signal<Usuario | undefined | null>(undefined);
@@ -31,8 +34,18 @@ export class AuthenticationService {
     localStorage.removeItem('token');
     localStorage.removeItem('identificacion');
     localStorage.removeItem('expirationToken');
+    localStorage.removeItem('rolUsuario');
     this.currentUserSignal.set(null);
     this._router.navigateByUrl('/authentication/login');
+  }
+
+  logoutByExpiration(): void {
+    // Limpiar el token almacenado en el localStorage
+    localStorage.removeItem('token');
+    localStorage.removeItem('identificacion');
+    localStorage.removeItem('expirationToken');
+    localStorage.removeItem('rolUsuario');
+    this.currentUserSignal.set(null);
   }
 
   isLoggedIn(): boolean {
@@ -48,7 +61,7 @@ export class AuthenticationService {
 
   isAdmin(): boolean {
     // Verifica si el usuario es administrador
-    return localStorage.getItem('rol_usuario') === 'ROLE_ADMIN';
+    return localStorage.getItem('rolUsuario') === 'ROLE_ADMIN';
   }
 
   isTokenExpired(): boolean {
@@ -59,7 +72,12 @@ export class AuthenticationService {
 
     const expirationDate = new Date(expirationToken).getTime();
     const now = new Date().getTime();
+    const isExpired = now >= expirationDate;
 
-    return now >= expirationDate;
+    if (isExpired) {
+      this._sessionExpiredDialogService.openDialog();
+    }
+
+    return isExpired;
   }
 }
